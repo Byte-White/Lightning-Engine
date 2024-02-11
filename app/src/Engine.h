36a50dd84dc2b547cae6b6b8338bbbcf898c58dc
Byte-Wh1te <bytewh1te@yahoo.com>
2024-02-11 12:05:15 +0200

    Theme setup

---
+++ app/src/Engine.h
@@ -0,0 +1,8 @@
+#include "Magma/Magma.h"
+
+class LightningEngine :public mg::Application
+{
+public:
+	void Init();
+	void Render();
+};
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,34 +1,11 @@
 #define MAGMA_ENTRYPOINT
 #include "Magma/Magma.h"
-
-class MyApplication:public mg::Application
-{
-public:
-	MyApplication()
-	{
-
-	}
-
-	void Init() override
-	{
-		SetSize(800,600);
-		SetTitle("Magma Application");
-	}
-
-	~MyApplication()
-	{
-
-	}
-	void Render() override
-	{
-		ImGui::Begin("Magma App Template");
-    		if(mg::renderer->GetAPI() == mg::RenderAPI::Vulkan) ImGui::Text("Render API: Vulkan");
-		else ImGui::Text("Render API: OpenGL");
-		ImGui::End();
-	}
-};
+#include "Engine.h"
 
 mg::Application* CreateApplication(int argc, char** argv)
 {
-	return new MyApplication();
+	LightningEngine* app = new LightningEngine();
+	app->SetTitle("Lightning Engine");
+	app->SetSize(1600, 800);
+	return app;
 }
